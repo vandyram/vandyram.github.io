87a50d919e58524f04a0a2547d856256269aeb28
Ramakrishnan Sathyavageeswaran <ramkrishs@outlook.com>
2015-10-24 12:27:49 -0500

    website

---
+++ js/flickr.js
@@ -0,0 +1,4 @@
+var apiurl,myresult,apiurl_size,selected_size;
+
+var apiurl,myresult,apiurl_size,selected_size;
+apiurl = "https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=dd6e24f9bd230da26862b01ca8b9700a&photoset_id=72157633030023508&user_id=91776516%40N05&format=json&nojsoncallback=1";
