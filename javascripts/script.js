bcc9455eb136bedc4f869dfe85113f182d411023
ramkrishs <ramkrishs@outlook.com>
2016-08-16 13:58:57 -0700

    Adding missing files

---
+++ javascripts/script.js
@@ -0,0 +1,15 @@
+/**
+ * Main Javascript File
+ *
+ * @author : Ramakrishnan Sathyavageeswaran
+ *
+ */
+
+(function () {
+
+    $(".tab").click(function () {
+        $(".tab").removeClass("active");
+        $(this).addClass("active");
+    });
+
+}());
